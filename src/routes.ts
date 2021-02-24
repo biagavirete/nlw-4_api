723bb1e732ae8f4dab5601db45c415d7f8766ead
biagavirete <biagavirete@gmail.com>
2021-02-24 14:27:24 -0300

    feat:create surveys models, controller, repository

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
+
 
 router.post("/users", userController.create);
+router.get("/surveys", surveyController.show);
+router.post("/surveys", surveyController.create);
 
 export { router }
