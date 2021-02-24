05375b28f07749bd417141f4f0b70c1d0b03c4fa
biagavirete <biagavirete@gmail.com>
2021-02-24 13:47:30 -0300

    feat: add create user method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
+
 class UserController {
-
   async create(request: Request, response: Response) {
-    const body = request.body;
-    console.log(body);
-    return response.send();
-  }
+    const { name, email } = request.body;
 
+    const usersRepository = getRepository(User)
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email
+    })
+
+    const user = usersRepository.create({
+      name,
+      email
+    });
+
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: "User already exists"
+      })
+    }
+
+    await usersRepository.save(user);
+
+    return response.json(user);
+  }
 }
 
 export { UserController }
