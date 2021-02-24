c7e13731f22b108463a33bf314ee5e505d3aa67f
biagavirete <biagavirete@gmail.com>
2021-02-24 14:26:39 -0300

    fix: add @entity

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -1,6 +1,7 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../models/User';
 
+@EntityRepository(User)
 class UsersRepository extends Repository<User> {
 
 }
