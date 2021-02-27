e6898b32f0601498cf3bd4f84c104c04b90a7a1c
biagavirete <biagavirete@gmail.com>
2021-02-26 22:02:04 -0300

    feat: survey-user models, repository and migration

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
@@ -7,9 +8,13 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 
+const sendMaisController = new SendMailController();
 
 router.post("/users", userController.create);
+
 router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
+
+router.post("/sendMail", sendMaisController.execute);
 
 export { router }
