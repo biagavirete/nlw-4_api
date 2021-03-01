734b7dd9b6c1e67c7660b06016339444b9ddcd21
biagavirete <biagavirete@gmail.com>
2021-03-01 16:34:06 -0300

    feat: create answer and nps controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
@@ -10,11 +12,19 @@ const surveyController = new SurveysController();
 
 const sendMaisController = new SendMailController();
 
+const answerController = new AnswerController();
+
+const npsController = new NpsController();
+
 router.post("/users", userController.create);
 
 router.get("/surveys", surveyController.show);
 router.post("/surveys", surveyController.create);
 
 router.post("/sendMail", sendMaisController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router }
